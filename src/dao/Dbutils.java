f2da17d10894270483fc4e7992d62341b8179332
linsola <linsola@outlook.com>
2018-11-28 14:22:58 +0900

    推送到本地仓库，本地仓库地址默认就是项目地址

---
+++ src/dao/Dbutils.java
@@ -0,0 +1,12 @@
+package dao;
+
+/**
+ * @ Package: dao
+ * @ Author     ：linsola
+ * @ Date       ：Created in 14:14 2018/11/28
+ * @ Description：封装与数据库相关的常用操作，如建立连接，释放连接
+ * @ Modified By：
+ * @ Version:
+ */
+public class Dbutils {
+}
---
+++ src/dao/UserDaoImpl.java
@@ -0,0 +1,16 @@
+package dao;
+
+import pojo.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ 实现接口
+ */
+public class UserDaoImpl implements UserDao {
+    @Override
+    public ArrayList queryAllUsers() {
+        return null;
+    }
+}
